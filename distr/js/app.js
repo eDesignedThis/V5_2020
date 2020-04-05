1407cf98fe7fa664d680b39daeea28b950210d50
Erika Wright <erikawright@administrators-MacBook-Pro.local>
2020-04-04 23:19:43 -0400

    Base-Structure

--- distr/js/app.js
+++ distr/js/app.js
@@ -1,1 +1,26 @@
 console.log("app js is running");
+
+$(window).scroll(function() {
+  $(".header--default").addClass("shadow");
+});
+
+$.ajax({
+  url: "../distr/js/main-nav.json",
+  success: function(links) {
+    for (i = 0; i < links.length; i++) {
+      $("ul.navbar-nav").append(
+        `<li class='nav-item'><a class='nav-link' href='/distr${links[i].url}'>${links[i].name}</a></li>`
+      );
+      $("li.active").removeClass("active");
+      $('a[href="' + location.pathname + '"]')
+        .closest("li")
+        .addClass("active");
+
+      let str = links[i].url;
+      if (window.location.href.indexOf(str) > 0) {
+        document.title = links[i].name + " | V5_2020";
+        $(".screen-title").append(links[i].name);
+      }
+    }
+  }
+});
